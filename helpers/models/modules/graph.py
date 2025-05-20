545e40175dea0067866fb14f81849362b60fe5e6
Martin Le Bras <martin.lebras0@gmail.com>
2025-05-20 14:45:21 +0200

    ml/graph module (GAT to test)

--- helpers/models/modules/graph.py
+++ helpers/models/modules/graph.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, GATv2Conv
 
 class GCN(nn.Module):
     def __init__(self, in_channels, hidden_channels=32):
@@ -21,4 +21,21 @@ class GCN(nn.Module):
         
         return x
     
-#TODO GAT
+class GAT(nn.Module):
+    def __init__(self, in_channels, heads_1=8, heads_2=1, hidden_channels=32):
+        super().__init__()
+        self.gat1 = GATv2Conv(in_channels=in_channels, out_channels=hidden_channels, heads=heads_1, concat=True)
+
+        self.gat2 = GATv2Conv(in_channels=hidden_channels*heads_1, out_channels=hidden_channels, heads=heads_2, concat=False)
+
+        self.activation = nn.GELU()
+        
+    def forward(self, x, edge_index):
+
+        x = self.gcn1(x, edge_index)
+        
+        x = self.activation(x)
+
+        x = self.gcn2(x, edge_index)
+
+        return x
